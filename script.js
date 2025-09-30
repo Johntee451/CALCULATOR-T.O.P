9d9423b557b395aa20e47e3aeb947ae78e8ab5d9
Johntee451 <johntee451@gmail.com>
2025-09-30 14:36:48 +0100

    first commit for the last foundations project(calculator). although i have been writing the code for days now, before making this first commit. the project is 98% complete. other changes and commit will be minor, since the major part of the project has beencompleted.

---
+++ script.js
@@ -0,0 +1,266 @@
+//additon Function:
+function add(num1,num2,operator) {
+    let result;
+    if (operator === "+") {
+        result = (num1) + (num2);
+        return(result);
+    }
+}
+
+//subtraction Function:
+function subtract(num1,num2,operator) {
+    let result;
+    if (operator === "-") {
+        result = (num1) - (num2);
+        return (result);
+    }
+}
+
+//multiplication Function:
+function multiply(num1,num2,operator) {
+    let result;
+    if (operator === "*") {
+        result = (num1) * (num2);
+        return (result);
+    }
+}
+
+//division Function:
+function divide(num1,num2,operator) {
+    let result;
+    if (operator === "/") {
+        result = (num1) / (num2);
+        return (result);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let number1;
+let number2;
+let sign;
+
+let displayParaDiv = document.querySelector("#displayParaDiv");
+let displayParaDiv1 = document.querySelector("#displayParaDiv1");
+
+let onSymbolDiv = document.querySelector("#onSymbolDiv");
+let offSymbolDiv = document.querySelector("#offSymbolDiv");
+let deleteSymbolDiv = document.querySelector("#deleteSymbolDiv");
+let clearSymbolDiv = document.querySelector("#clearSymbolDiv");
+
+let button1Div = document.querySelector("#button1Div");
+let button2Div = document.querySelector("#button2Div");
+let button3Div = document.querySelector("#button3Div");
+let button4Div = document.querySelector("#button4Div");
+let button5Div = document.querySelector("#button5Div");
+let button6Div = document.querySelector("#button6Div");
+let button7Div = document.querySelector("#button7Div");
+let button8Div = document.querySelector("#button8Div");
+let button9Div = document.querySelector("#button9Div");
+let button0Div = document.querySelector("#button0Div");
+let button00Div = document.querySelector("#button00Div");
+let buttonDotDiv = document.querySelector("#buttonDotDiv");
+
+let signButtonAdditionDiv = document.querySelector("#signButtonAdditionDiv");
+let signButtonSubtractionDiv = document.querySelector("#signButtonSubtractionDiv");
+let signButtonMultiplicationDiv = document.querySelector("#signButtonMultiplicationDiv");
+let signButtonDivisionDiv = document.querySelector("#signButtonDivisionDiv");
+let signButtonEqualtoDiv = document.querySelector("#signButtonEqualtoDiv");
+
+
+
+//number buttons
+button1Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button1Div.textContent;
+})
+button2Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button2Div.textContent;
+})
+button3Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button3Div.textContent;
+})
+button4Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button4Div.textContent;
+})
+button5Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button5Div.textContent;
+})
+button6Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button6Div.textContent;
+})
+button7Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button7Div.textContent;
+})
+button8Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button8Div.textContent;
+})
+button9Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button9Div.textContent;
+})
+button0Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button0Div.textContent;
+})
+button00Div.addEventListener("click", function(){
+    displayParaDiv.textContent += button00Div.textContent;
+})
+buttonDotDiv.addEventListener("click", function(){
+    displayParaDiv.textContent += buttonDotDiv.textContent;
+})
+
+
+
+
+
+
+//symbol buttons
+onSymbolDiv.addEventListener("click", function(){
+    displayParaDiv.textContent = "CALCULATOR IS ON";
+    console.log("CALCULATOR IS ON");
+})
+offSymbolDiv.addEventListener("click", function(){
+    displayParaDiv.textContent = "CALCULATOR IS OFF";
+    console.log("CALCULATOR IS OFF");
+})
+deleteSymbolDiv.addEventListener("click", function(){
+    let dispCont = displayParaDiv.textContent;
+    let arrDisp = dispCont.split("");
+    arrDisp.pop();
+    let joinDisp = arrDisp.join("");
+    displayParaDiv.textContent = joinDisp;
+})
+clearSymbolDiv.addEventListener("click", function(){
+    displayParaDiv.textContent = "";
+    displayParaDiv1.textContent = "";
+})
+
+
+
+
+
+//sign input buttons
+signButtonAdditionDiv.addEventListener("click", function(){
+    let displayContent = displayParaDiv.textContent;
+    if (displayContent !== "" && displayContent[displayContent.length-1] !== "-" && displayContent[displayContent.length-1] !== "+" && displayContent[displayContent.length-1] !== "*" && displayContent[displayContent.length-1] !== "/") {
+        displayParaDiv.textContent += signButtonAdditionDiv.textContent;
+    }
+    else if (displayContent[displayContent.length-1] === "-" || displayContent[displayContent.length-1] === "+" || displayContent[displayContent.length-1] === "*" || displayContent[displayContent.length-1] === "/") {
+        let dispCont = displayParaDiv.textContent;
+        let arrDisp = dispCont.split("");
+        arrDisp.pop();
+        arrDisp.push(signButtonAdditionDiv.textContent);
+        let joinDisp = arrDisp.join("");
+        displayParaDiv.textContent = joinDisp;
+    }
+})
+
+signButtonSubtractionDiv.addEventListener("click", function(){
+    let displayContent = displayParaDiv.textContent;
+    if (displayContent[displayContent.length-1] !== "-" && displayContent[displayContent.length-1] !== "+") {
+        displayParaDiv.textContent += signButtonSubtractionDiv.textContent;
+    }
+    else if (displayContent[displayContent.length-1] === "-" || displayContent[displayContent.length-1] === "+") {
+        let dispCont = displayParaDiv.textContent;
+        let arrDisp = dispCont.split("");
+        arrDisp.pop();
+        arrDisp.push(signButtonSubtractionDiv.textContent);
+        let joinDisp = arrDisp.join("");
+        displayParaDiv.textContent = joinDisp;
+    }
+})
+
+signButtonMultiplicationDiv.addEventListener("click", function(){
+    let displayContent = displayParaDiv.textContent;
+    if (displayContent !== "" && displayContent[displayContent.length-1] !== "-" && displayContent[displayContent.length-1] !== "+" && displayContent[displayContent.length-1] !== "*" && displayContent[displayContent.length-1] !== "/") {
+        displayParaDiv.textContent += signButtonMultiplicationDiv.textContent;
+    }
+    else if (displayContent[displayContent.length-1] === "-" || displayContent[displayContent.length-1] === "+" || displayContent[displayContent.length-1] === "/" || displayContent[displayContent.length-1] === "*") {
+        let dispCont = displayParaDiv.textContent;
+        let arrDisp = dispCont.split("");
+        arrDisp.pop();
+        arrDisp.push(signButtonMultiplicationDiv.textContent);
+        let joinDisp = arrDisp.join("");
+        displayParaDiv.textContent = joinDisp;
+    }
+})
+
+signButtonDivisionDiv.addEventListener("click", function(){
+    let displayContent = displayParaDiv.textContent;
+    if (displayContent !== "" && displayContent[displayContent.length-1] !== "-" && displayContent[displayContent.length-1] !== "+" && displayContent[displayContent.length-1] !== "*" && displayContent[displayContent.length-1] !== "/") {
+        displayParaDiv.textContent += signButtonDivisionDiv.textContent;
+    }
+    else if (displayContent[displayContent.length-1] === "-" || displayContent[displayContent.length-1] === "+" || displayContent[displayContent.length-1] === "/" || displayContent[displayContent.length-1] === "*") {
+        let dispCont = displayParaDiv.textContent;
+        let arrDisp = dispCont.split("");
+        arrDisp.pop();
+        arrDisp.push(signButtonDivisionDiv.textContent);
+        let joinDisp = arrDisp.join("");
+        displayParaDiv.textContent = joinDisp;
+    }
+})
+
+
+
+
+
+
+
+
+
+signButtonEqualtoDiv.addEventListener("click", function(){
+    let displayContent = displayParaDiv.textContent;
+
+    if (displayContent.includes("-", 1) && !(displayContent.includes("*-", 1)) && !(displayContent.includes("/-", 1))) {
+        let signIndex = displayContent.indexOf("-", 1);
+        number1 = displayContent.slice(0, signIndex);
+        sign = displayContent[signIndex];
+        number2 = displayContent.slice(signIndex+1, displayContent.length);
+
+        displayParaDiv.textContent = subtract(Number(number1),Number(number2),sign);
+        displayParaDiv1.textContent = subtract(Number(number1),Number(number2),sign);
+        console.log(subtract(Number(number1),Number(number2),sign));
+    }
+
+    else if (displayContent.includes("+", 1)) {
+        let signIndex = displayContent.indexOf("+", 1);
+        number1 = displayContent.slice(0, signIndex);
+        sign = displayContent[signIndex];
+        number2 = displayContent.slice(signIndex+1, displayContent.length);
+
+        displayParaDiv.textContent = add(Number(number1),Number(number2),sign);
+        displayParaDiv1.textContent = add(Number(number1),Number(number2),sign);
+        console.log(add(Number(number1),Number(number2),sign));
+    }
+
+    else if (displayContent.includes("*", 1)) {
+        let signIndex = displayContent.indexOf("*", 1);
+        number1 = displayContent.slice(0, signIndex);
+        sign = displayContent[signIndex];
+        number2 = displayContent.slice(signIndex+1, displayContent.length);
+
+        displayParaDiv.textContent = multiply(Number(number1),Number(number2),sign);
+        displayParaDiv1.textContent = multiply(Number(number1),Number(number2),sign);
+        console.log(multiply(Number(number1),Number(number2),sign));
+    }
+
+    else if (displayContent.includes("/", 1)) {
+        let signIndex = displayContent.indexOf("/", 1);
+        number1 = displayContent.slice(0, signIndex);
+        sign = displayContent[signIndex];
+        number2 = displayContent.slice(signIndex+1, displayContent.length);
+
+        displayParaDiv.textContent = divide(Number(number1),Number(number2),sign);
+        displayParaDiv1.textContent = divide(Number(number1),Number(number2),sign);
+        console.log(divide(Number(number1),Number(number2),sign));
+    }
+    
+})
